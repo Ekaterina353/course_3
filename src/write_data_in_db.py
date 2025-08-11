120ee8557aff4bcad15c94b558467afbf587b850
Ekateryna <gorbunovakatja530@gmail.com>
2025-08-11 13:15:50 +0300

    .env

--- src/write_data_in_db.py
+++ src/write_data_in_db.py
@@ -9,7 +9,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
 
 
 class DB:
@@ -23,7 +27,7 @@ class DB:
         """Подключение к базам данных"""
         try:
             self.__conn = psycopg2.connect(
-                database="vacancy", user="postgres", password=PASSWORD, host="localhost", port="5432"
+                database=DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST, port=DB_PORT
             )
             self.__conn.set_session(autocommit=True)
             self.__cur = self.__conn.cursor()
